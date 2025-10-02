2720b91c496aed165b19712229700a83901618bd
Marcos Cazzulino <marcoscazzulino@gmail.com>
2025-10-02 09:50:46 -0300

    filterProducts

--- react-actividad/src/components/FilterProducts.jsx
+++ react-actividad/src/components/FilterProducts.jsx
@@ -16,27 +16,44 @@ const productos = [
 ];
 
 function FilterProducts(){
-    const [filtro, setFiltro] = useState('')
+    const [filtro, setFiltro] = useState('todos')
 
-    const Filtrar = (categoria) => {
-        setFiltro(categoria)
-    }
+    const productosFiltrados = filtro === "todos" ? productos : productos.filter(p => p.categoria === filtro)
 
     return (
         <div className="section-productos">
             <h2>Productos</h2>
-            <button onClick={Filtrar("todos")}>Todos</button>
-            <button onClick={Filtrar("electronica")}>Electrónica</button>
-            <button onClick={Filtrar("ropa")}>Ropa</button>
-            <button onClick={Filtrar("libros")}>Libros</button>
+            <div>
+                <button onClick={() => setFiltro('todos')}>Todos</button>
+                <button onClick={() => setFiltro('electronica')}>Electrónica</button>
+                <button onClick={() => setFiltro('ropa')}>Ropa</button>
+                <button onClick={() => setFiltro('libros')}>Libros</button>
+            </div>
+
+            {filtro !== "todos" && (
+                <p>Productos: {filtro}</p>
+            )}
+
+            {productosFiltrados.length === 0 ? (
+                <p>No hay productos en esta categoría</p>
+            ) : (
+                <>
+                    <p>Hay {productosFiltrados.length}{" "}
+                        {productosFiltrados.length === 1 ? "producto" : "productos"} disponibles
+                    </p>
+                </>
+            )}
+
             <ul className="lista-productos">
-                {productos.map(producto => (
+                {productosFiltrados.map(producto => (
                     <li key={producto.id}>
                         <Products nombre={producto.nombre} categoria={producto.categoria} precio={producto.precio}/>
+                        {producto.precio > 500 && (
+                            <span className="premium">Producto premium</span>
+                        )}
                     </li>
                 ))}
             </ul>
-            {if ()}
         </div>
     )
 }
